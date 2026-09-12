e4de1b27b463526a2dbf3f0873865a9bda1cc51a
Ehfend552 <ehfend521@gmail.com>
2026-09-12 16:17:45 +0700

    Update: Form WA + Hapus gambar lama + tambah logo client

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,17 @@
-// Navbar Fixed
-window.onscroll = function () {
-  const header = document.querySelector("header");
-  const fixedNav = header.offsetTop;
-
-  if (window.pageYOffset > fixedNav) {
-    header.classList.add("navbar-fixed");
-  } else {
-    header.classList.remove("navbar-fixed");
-  }
-};
-// Hamburger
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
+// 1. Klik hamburger buat buka tutup
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// 2. Klik menu buat nutup otomatis
+const navLinks = document.querySelectorAll("#nav-menu a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html.js}"],
+  content: ["./*.html"],
+  safelist: [
+    "hamburger-active", // <-- TAMBAHIN INI
+  ],
   theme: {
-    container: {
-      center: true,
-      padding: "16px",
-    },
     extend: {
       colors: {
         primary: "#14b8a6",
         secondary: "#64748b",
         dark: "#0f172a",
-      },
-      screens: {
-        "2xl": "1320px",
       },
     },
   },
