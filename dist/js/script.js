bec0f0ee48f3ed9620d7a76803349c249ce8a8cb
Ehfend552 <ehfend521@gmail.com>
2026-09-12 20:12:06 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
+const toTop = document.querySelector("#to-top");
 
 // 1. Klik hamburger buat buka tutup
 hamburger.addEventListener("click", function () {
@@ -14,4 +15,20 @@ navLinks.forEach((link) => {
     hamburger.classList.remove("hamburger-active");
     navMenu.classList.add("hidden");
   });
+});
+// 3. Munculin tombol pas scroll
+window.onscroll = function () {
+  if (window.scrollY > 300) {
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
+
+// 4. Klik tombol buat scroll ke atas
+toTop.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
